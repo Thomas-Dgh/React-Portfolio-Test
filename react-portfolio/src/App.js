5124b753120506858f0f2db191666817bdc6f671
Thomas-Dgh <thomas.dgh@icloud.com>
2022-09-01 11:52:13 +0200

    add some work

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -2,19 +2,22 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
+import Home from './components/Home';
 
 function App() {
   return (
-<>
+    <>
 
-<Routes>
-<Route path="/" element={<Layout />}/>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+       <Route index element={<Home />} />
+       
+       </Route>
+
+      </Routes>
 
 
-</Routes>
-
-
-</>
+    </>
   )
 }
 
